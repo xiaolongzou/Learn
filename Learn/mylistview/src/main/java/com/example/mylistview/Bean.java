504d920058e176e59894e834476616fa6f363ff1
xiaolong <1539721254@qq.com>
2021-11-14 22:51:32 +0800

    feat: ListView

---
+++ Learn/mylistview/src/main/java/com/example/mylistview/Bean.java
@@ -0,0 +1,13 @@
+package com.example.mylistview;
+
+public class Bean {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
